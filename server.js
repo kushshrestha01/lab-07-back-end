1818bf6a88458b24f26929d2e60182695a76eb3b
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-05-01 11:06:14 -0700

    feature 2 complete

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const superagent = require('superagent');
 //enviroment variable defined in .env as 3000
 const PORT = process.env.PORT || 3000;
 
-console.log(process.env.TEST);
 
 //if front end is in same directory, front end is in public file and following line is needed
 //app.use(express.static('./public'));
@@ -30,12 +29,13 @@ app.get('/TEST', (request, response) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
+//Following codes have been referenced to Michelle's class lecture:
 app.get('/location', (request, response) => {
   try {
     const queryData = request.query.data;
     let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${queryData}&key=${process.env.GEOCODE_API_KEY}`;
+    
     superagent.get(geocodeURL)
       .end( (err, googleMapsApiResponse) => {
         const location = new Location(queryData, googleMapsApiResponse.body);
@@ -56,42 +56,18 @@ function Location(query, res) {
   this.longitude = res.results[0].geometry.location.lng;
 }
 
-// app.get('/location', (request, response) => {
-//   try {
-//   //   const queryDate = request.query.data;
-//   //   let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${queryData}&key=${process.env.GEOCODE_API_KEY}`;
-//   //  superagent.get(geocodeURL)
-
-
-//     let jsonInfo = require('./data/geo.json');
-//     let someLocation = new GeoObject(jsonInfo.results[0].address_components[0].long_name, jsonInfo.results[0].formatted_address, jsonInfo.results[0].geometry.location.lat, jsonInfo.results[0].geometry.location.lng);
-//     response.send(someLocation);
-//   } catch (error) {
-//     console.error(error);
-//     response.status(500).send('Status: 500. So sorry, something went wrong');
-//   }
-// });
-
-function GeoObject(query, address, latitude, longitude) {
-  this.formatted_query = query;
-  this.formatted_address = address;
-  this.latitude = latitude;
-  this.longitude = longitude;
-}
 
 app.get('/weather', (request, response) => {
   try {
     let jsonInfo = require('./data/darksky.json');
     let weatherDates = [];
-    weatherDates = jsonInfo.daily.data.map((extractedData) => ({
-      forecast: extractedData.summary,
-      time: extractedData.time
-    }));
+    weatherDates = jsonInfo.daily.data.map((extractedData) => {
+      let forecast= extractedData.summary;
+      let time =  extractedData.time;
+      return new WeatherObject(forecast, time);
+    });
 
-    // for (let index = 0; index < jsonInfo.daily.data.length; index++) {
-    //   let tempObj = new WeatherObject(jsonInfo.daily.data[index].summary, jsonInfo.daily.data[index].time);
-    //   weatherDates.push(tempObj);
-    // }
+
 
     response.send(weatherDates);
   } catch (error) {
@@ -100,7 +76,10 @@ app.get('/weather', (request, response) => {
   }
 });
 
-// function WeatherObject(forecast, time) {
-//   this.forecast = forecast;
-//   this.time = new Date(time * 1000).toDateString();
-// }
+function WeatherObject(forecast, time) {
+  this.forecast = forecast;
+  this.time = new Date(time * 1000).toDateString();
+}
+
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
